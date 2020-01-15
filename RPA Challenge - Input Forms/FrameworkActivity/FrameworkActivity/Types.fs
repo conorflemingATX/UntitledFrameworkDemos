cff537692a3b77492cf72808b09717c89bcf74c1
Conor Fleming <conorfleming@outlook.com>
2020-01-14 19:19:33 -0600

    modelling types, so far so good

--- RPA Challenge - Input Forms/FrameworkActivity/FrameworkActivity/Types.fs
+++ RPA Challenge - Input Forms/FrameworkActivity/FrameworkActivity/Types.fs
@@ -5,6 +5,9 @@ open FSharp.Interop.Excel
 [<Literal>]
 let ConfigPath = @"C:\Users\cfleming\dev\UntitledUiPathDemos\RPA Challenge - Input Forms\data\Config.xlsx"
 
+[<Literal>]
+let ChallengePath = @"C:\Users\cfleming\dev\UntitledUiPathDemos\RPA Challenge - Input Forms\data\Challenge.xlsx"
+
 type NotImplemented = exn
 
 (* ===========
@@ -13,6 +16,7 @@ type NotImplemented = exn
 
 type Config = ExcelFile<ConfigPath>
 
+type UnvalidatedConfig = Config.Row
 
 type FormUrl =
     private
@@ -44,8 +48,82 @@ type ValidatedConfig =
     { FormUrl: FormUrl
       MaxRetries: MaxRetries }
 
-type UnvalidatedConfig = Config.Row
+type ValidateConfig = Config.Row -> ValidatedConfig
 
 type GetConfig = unit -> UnvalidatedConfig
 
-type ValidateConfig = Config.Row -> ValidatedConfig
+
+(* ===========
+     Challenge Data
+   =========== *)
+
+// TODO: define the create and validate functions on the constrained simple types, and think about how to handle inputs that fail validation.
+
+type Challenge = ExcelFile<ChallengePath>
+
+type UnvalidatedFormInput = Challenge.Row
+
+type FirstName = Undefined
+
+and FirstNameError = FirstNameError of string
+
+type LastName = Undefined
+
+and LastNameError = LastNameError of string
+
+type CompanyName = Undefined
+
+and CompanyNameError = CompanyNameError of string
+
+type Role = Undefined
+
+and RoleError = RoleError of string
+
+type Address = Undefined
+
+and AddressError = AddressError of string
+
+type Email = Undefined
+
+and EmailError = EmailError of string
+
+type PhoneNumber = Undefined
+
+and PhoneNumberError = PhoneNumberError of string
+
+type FormInputValidationError =
+    | FirstNameError
+    | LastNameError
+    | CompanyNameError
+    | RoleError
+    | AddressError
+    | EmailError
+    | PhoneNumberError
+
+type ValidatedFormInput =
+    { FirstName: FirstName
+      LastName: LastName
+      CompanyName: CompanyName
+      Role: Role
+      Address: Address
+      Email: Email
+      PhoneNumber: PhoneNumber }
+
+type InvalidFormInput =
+    { RowNumber: int
+      Errors: FormUrlValidationError list }
+
+(* ===========
+     State
+   =========== *)
+
+type Init = Init
+
+type Configured =
+    { Config: ValidatedConfig }
+
+type ReadFile =
+    { Config: ValidatedConfig
+      FormInput: ValidatedFormInput list
+      InvalidFormInput: InvalidFormInput list }
+// What are the steps of for the rest of process?
